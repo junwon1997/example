9ef40207e640466028f0314ce9c3d34d384690fd
junwon1997 <junwon20170339@gmail.com>
2023-07-13 10:38:27 +0900

    테스트 프로젝트 메인 메서드 수정 2탄

--- Testprj/src/HelloWorld.java
+++ Testprj/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Hello Spring");
+		System.out.println("Hello python");
 	}
 }
